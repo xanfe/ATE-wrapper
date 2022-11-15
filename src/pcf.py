c308380aeccb943279d7b4b897a6d25035d8cc92
Felix Sanchez <felixsanchez0124@gmail.com>
2022-11-15 14:44:20 -0600

    pcf class to handle part number test spec file

---
+++ src/pcf.py
@@ -0,0 +1,39 @@
+from io import StringIO
+import pandas as pd
+import re
+
+
+
+class Section:
+    def __init__(self, name, str_data) -> None:
+        self.name = name
+        self.df = pd.read_csv(StringIO(str_data), sep=',')
+        self.column_names = self.df.columns
+        self.records = self.df.to_records()
+    
+    def __repr__(self) -> str:
+        return str(self.df.head())
+        
+
+
+
+
+
+class Pcf:
+    def __init__(self, path) -> None:
+        self.path = path
+        self.sections = []
+        self._set_sections()
+
+
+    def _get_raw_sections(self):
+
+        with open(self.path) as pcf:
+            pattern = r'\n,{0,20}\n'
+            return re.split(pattern, pcf.read())
+
+    def _set_sections(self):
+        
+        for raw_section in self._get_raw_sections():
+            name_w_data = raw_section.split('\n', 1)
+            self.sections.append(Section(name_w_data[0], name_w_data[1]))
