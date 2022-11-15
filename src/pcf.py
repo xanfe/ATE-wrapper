fbab4e22fa454a41ed84ce2eafc91ce16fb25a96
Felix Sanchez <felixsanchez0124@gmail.com>
2022-11-15 15:09:38 -0600

    make suscriptable pcf

--- src/pcf.py
+++ src/pcf.py
@@ -4,19 +4,15 @@ import re
 
 
 
-class Section:
+class Section():
     def __init__(self, name, str_data) -> None:
-        self.name = name
+        self.name:str = name
         self.df = pd.read_csv(StringIO(str_data), sep=',')
         self.column_names = self.df.columns
         self.records = self.df.to_records()
     
     def __repr__(self) -> str:
         return str(self.df.head())
-        
-
-
-
 
 
 class Pcf:
@@ -24,16 +20,19 @@ class Pcf:
         self.path = path
         self.sections = []
         self._set_sections()
-
+    
+    def __getitem__(self, key) -> Section:
+        for section in self.sections:
+            if key in section.name:
+                return section
+        raise KeyError(key)
 
     def _get_raw_sections(self):
-
         with open(self.path) as pcf:
             pattern = r'\n,{0,20}\n'
             return re.split(pattern, pcf.read())
 
     def _set_sections(self):
-        
         for raw_section in self._get_raw_sections():
             name_w_data = raw_section.split('\n', 1)
             self.sections.append(Section(name_w_data[0], name_w_data[1]))
