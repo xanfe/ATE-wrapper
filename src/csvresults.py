191b790d472022134424de3cc4a42a35276d68de
Felix Sanchez <felixsanchez0124@gmail.com>
2022-11-25 14:37:32 -0600

    basic test properties

--- src/csvresults.py
+++ src/csvresults.py
@@ -4,14 +4,10 @@ from pandas import DataFrame
 import re
 
 from dataclasses import dataclass
-
 from typing import *
-
 from pcf import Pcf
 
 DATE_COLUMN_NAME = '[TIME] TIMESTAMP / RECORD ID'
-
-
 
 class MyCsv:
 
@@ -24,8 +20,6 @@ class MyCsv:
     @property
     def records(self) -> list[Record]:
         return [self.Record(self, self.df.iloc[idx]) for idx in self.df.index]
-
-
 
     def _to_datetime(self):
         self.df[DATE_COLUMN_NAME] = pd.to_datetime(self.df[DATE_COLUMN_NAME], format='%Y-%m-%d_%H:%M:%S.%f')
@@ -46,11 +40,10 @@ class MyCsv:
             self.date = data.get(DATE_COLUMN_NAME)
         
         @property
-        def status(self) -> bool:
+        def passed(self) -> bool:
             try:
                 return True if self.data["[RESULT] TEST P/F STATUS"] == 'PASS' else False
             finally:
-                print("dropped")
                 self.data = self.data.drop("[RESULT] TEST P/F STATUS")
 
         @property
@@ -71,50 +64,75 @@ class MyCsv:
 
         def get_failstring(self):
             for test in self._get_failed_tests():
-                print("limits for failed test: ", test.name)
+                print("test name: ", test.name)
+                print("test type: ", test.type)
+                print("test status: ", test.status)
+                print("test limits: ", test.limits)
+                print("test units: ", test.units)
         
         
 
 
         class Test:
 
-            def __init__(self, outer, name:str, data) -> None:
+            def __init__(self, outer:MyCsv.Record, name:str, data) -> None:
                 self._name = name
                 self.data = data
+                
                 if not isinstance(outer, MyCsv.Record):
                     raise TypeError(f"exp type: {MyCsv.Record}, recv type {type(outer)}")
-                self._outer = outer
-            
-            @property
-            def status(self) -> bool:
-                pass
+                self.record = outer
+                self.metadata:DataFrame = self.record.results.pcf["TEST RESULTS SPECIFICATIONS"].get_rows(self.name)
 
-            
+
             @property
             def name(self) -> str:
                 return re.split(r' LOW \[| HIGH \[| \[', self._name)[0]
+
+            @property
+            def status(self) -> bool:
+                return True if 'PASS' in self.data else False
             
             @property
-            def record(self) -> MyCsv.Record:
-                return self._outer
-
+            def type(self) -> str:
+                return self._get_attr("Data Type")
+            
             @property
-            def t_type(self):
-                pass
-
+            def nominal(self) -> float:
+                 return self._get_attr("Nominal")
+            
+            @property
+            def units(self) -> str:
+                return self._get_attr("Units")
             
             @property
             def limits(self) -> Limits:
-                return self.record.results.pcf["TEST RESULTS SPECIFICATIONS"].get_related_rows(self.name)
+                if self.type == "DBL" and len(self.metadata) > 1:
+                    low_series = self.metadata.loc[self.metadata['Step ID'] == self.name + ' LOW']
+                    high_series = self.metadata.loc[self.metadata['Step ID'] == self.name + ' HIGH']
+                    low_l =  float(low_series["Nominal"] - low_series["Tolerance"])
+                    high_l = float(high_series["Nominal"] + high_series["Tolerance"])
+                    return self.Limits(low_l, high_l)
 
 
+            
+            def __repr__(self) -> str:
+                return f'{self.name} : {self.data}'
+            
+            def _get_attr(self, attr_name):
+                if attr_name in self.metadata:
+                    return self.metadata[attr_name].values[0]
+                else:
+                    raise Exception(f"the metadata for this test does not containt '{attr_name}'")
+
+            
             @dataclass
             class Limits:
                 low: float
                 high:float
-            
-            def __repr__(self) -> str:
-                return f'{self.name} : {self.data}'
+
+                def __repr__(self) -> str:
+                    return f'low: {self.low} high: {self.high}'
     
     
     
