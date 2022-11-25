dea2e38ea9ecf82b1f0d0e8ebcb2528fc6663086
Felix Sanchez <felixsanchez0124@gmail.com>
2022-11-25 14:48:01 -0600

    basic main structure

--- src/main.py
+++ src/main.py
@@ -3,34 +3,32 @@
 import configparser
 from datetime import datetime
 import time
+import sys
 
-#local modles
+#local modules
 from pcf import Pcf
-from resultscan import CsvResults
+from csvresults import MyCsv
 
 def main():
 
     pcf = Pcf("settings\headers_info.pcf")
+    my_csv = MyCsv("data\sample\input.csv", pcf)
 
-    # print(pcf["TEST RESULTS SPECIFICATIONS"])
-
-    results = CsvResults("data\sample\input.csv")
 
     temp_curr_date = datetime.now()
     print(temp_curr_date)
 
-    last_record = results.get_last_record()
+    last_record = my_csv.get_last_record()
 
     while temp_curr_date > last_record.date:
-        print("aun no hay nada")
-
+        print("waiting for new record...")
         time.sleep(1)
-        last_record = results.get_last_record()
+        last_record = my_csv.get_last_record()
 
 
-    last_record.get_failstring()
+    if not last_record.passed: 
+        print("fails: \n", last_record.get_failstring())
 
-    #print("se escribio algo !")
 
 if __name__ == "__main__":
     main()
